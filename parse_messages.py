fea254046ffd2fdd9eb5e168582d972a030c6c5f
Prisha Bhatia <pbhatia@olin.edu>
2023-10-31 19:54:08 -0400

    Initial function for serial communication

--- parse_messages.py
+++ parse_messages.py
@@ -1,6 +1,15 @@
 ## serial communication jazz
+import serial
 
-from model import Model
+# from model import Model
 
-def parse_message():
-    
+# def parse_message():
+
+if __name__ == "__main__":
+    ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
+    ser.reset_input_buffer()
+
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode("utf-8").rstrip()
+            print(line)
