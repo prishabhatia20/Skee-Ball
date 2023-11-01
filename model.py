538195142af67399455428f96508efe4b6914b1f
prishabhatia20 <123204625+prishabhatia20@users.noreply.github.com>
2023-11-01 16:56:46 -0400

    Add methods to update score and number of tries left

--- model.py
+++ model.py
@@ -11,23 +11,36 @@ class Model:
     are different numbers, it keeps track of the state, and accordingly displays
     the appropriate sprites.
 
-    Attributes:
-        score: an integer representing the score of the player
-        num_tries: an integer representing how many times the
-        player has rolled & scored
-        total_tries: an integer representing how many total tries
-        the player has
     """
 
-    score = 0
-    num_tries = 0
     total_tries = 8
 
-    def __init__(self, player):
+    def __init__(self):
         """
         Initialize an instance of the Model class
 
         Args:
-            player: an instance of the
+            score: an integer representing the score of the player
+            num_tries: an integer representing how many times the
+            player has rolled & scored
+            total_tries: an integer representing how many total tries
+            the player has
 
         """
+        self.score = 0
+        self.num_tries = 0
+        self.active = True
+        self.tries_left = 8
+
+    def update_score(self, parsed_sensor_val):
+        """
+        This method updates the score when there is input from the IDE
+        """
+
+        self.score += parsed_sensor_val
+
+    def update_tries(self):
+        """
+        This method updates the number tries the user has left
+        """
+        self.tries_left = self.total_tries - self.num_tries
