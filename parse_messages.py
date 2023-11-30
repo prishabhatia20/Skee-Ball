240b470ef6523ddd7d5699a6721686a005618a1f
prishabhatia20 <pbhatia@olin.edu>
2023-11-30 14:24:50 -0500

    Update parse_messages to correctly read from serial monitor

--- parse_messages.py
+++ parse_messages.py
@@ -1,19 +1,34 @@
-## serial communication jazz
+"""
+File containing parse messages function
+"""
 import serial
 
 from model import Model
 
-def parse_message(arduino_port, baud_rate, model):
-    scores = [100, 100, 50, 40, 30, 10, 0]
+def parse_message(arduino_port, baud_rate):
+    """
+    Return what the player most recently scored
+
+    Args: 
+        arduino_port: a String representing the port the Arduino is
+        connected from
+
+        baud_rate: an integer representing the baud_rate
+    
+    Returns:
+        an integer representing how much to increment the score by
+    """
+    scores = [100, 50, 40, 30, 10, 0]
     score = 0
 
     try:
         arduino = serial.Serial(arduino_port, baud_rate, timeout=1)
     except:
-        print("Please check the port")
+        print("Please check the port and baud rate")
 
-    raw_data = arduino.readline().decode()
-    data = raw_data.rstrip("\"")
+    raw_data = arduino.readline()
+    string_data = raw_data.decode()
+    data = string_data[0: len(scores)]
 
     for i in range(0, len(scores)):
         if data[i] == 1:
